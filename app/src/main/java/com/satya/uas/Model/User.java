d6144cf08aacc2f15ed81a9ba4a95117733dc360
achmadsatyap <satyapradana98@gmail.com>
2019-12-22 16:34:37 +0700

    Membuat model user, security, dan menyambungkan regis ke database

---
+++ app/src/main/java/com/satya/uas/Model/User.java
@@ -0,0 +1,57 @@
+package com.satya.uas.Model;
+
+public class User {
+    public String username,nama, password, alamat, email;
+
+    public User() {
+
+    }
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getNama() {
+        return nama;
+    }
+
+    public void setNama(String nama) {
+        this.nama = nama;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
+
+    public String getAlamat() {
+        return alamat;
+    }
+
+    public void setAlamat(String alamat) {
+        this.alamat = alamat;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    public User(String username, String nama, String password, String alamat, String email) {
+        this.username = username;
+        this.nama = nama;
+        this.password = password;
+        this.alamat = alamat;
+        this.email = email;
+    }
+}
--- app/src/main/java/com/satya/uas/activity/RegistarsiActivity.java
+++ app/src/main/java/com/satya/uas/activity/RegistarsiActivity.java
@@ -2,15 +2,75 @@ package com.satya.uas.activity;
 
 import androidx.appcompat.app.AppCompatActivity;
 
+import android.content.Intent;
 import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.satya.uas.Model.Security;
+import com.satya.uas.Model.User;
 import com.satya.uas.R;
 
 public class RegistarsiActivity extends AppCompatActivity {
+
+    EditText tUsername, tName, tPassword, tAlamat, tEmail;
+    Button btnRegis;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_registarsi);
+
+        tUsername = findViewById(R.id.edit_username);
+        tName = findViewById(R.id.edt_name);
+        tPassword = findViewById(R.id.edt_password);
+        tAlamat = findViewById(R.id.edt_alamat);
+        tEmail = findViewById(R.id.edt_email);
+        btnRegis = findViewById(R.id.btn_req);
+
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+
+        btnRegis.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                addUser();
+            }
+        });
+    }
+
+    private void addUser() {
+        final String username = tUsername.getText().toString();
+        final String nama = tName.getText().toString();
+        final String password = tPassword.getText().toString();
+        final String alamat = tAlamat.getText().toString();
+        final String email = tEmail.getText().toString();
+
+        if (username.isEmpty() || nama.isEmpty() || password.isEmpty() || alamat.isEmpty() || email.isEmpty()){
+            showMessage("Mohon isi semua kolom ");
+        }else {
+            User user= new User(username,nama,password,email,alamat);
+            databaseReference.child(username).child("username").setValue(username.toString());
+            databaseReference.child(username).child("nama").setValue(nama.toString());
+            databaseReference.child(username).child("email").setValue(email.toString());
+            databaseReference.child(username).child("alamat").setValue(alamat.toString());
+            try {
+                databaseReference.child(username).child("password").setValue(Security.encrypt(password));
+            } catch (Exception e) {
+                e.printStackTrace();
+            }
+            Toast.makeText(getApplication(),"Success",Toast.LENGTH_LONG).show();
+            Intent intent = new Intent(getApplication(),LoginActivity.class);
+            startActivity(intent);
+        }
+    }
+
+    private void showMessage(String message) {
+        Toast.makeText(getApplicationContext(),message,Toast.LENGTH_LONG).show();
     }
 }
+
