ab6cdbe336954696120e94ff8f9bb9e008c087d3
achmadsatyap <satyapradana98@gmail.com>
2019-12-22 19:27:17 +0700

    Menyambungkan login ke firebase, dan membuat welcom slide

--- app/src/main/java/com/satya/uas/activity/LoginActivity.java
+++ app/src/main/java/com/satya/uas/activity/LoginActivity.java
@@ -1,19 +1,83 @@
 package com.satya.uas.activity;
 
+import androidx.annotation.NonNull;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.content.Intent;
 import android.os.Bundle;
+import android.util.Log;
 import android.view.View;
+import android.widget.Button;
+import android.widget.EditText;
+import android.widget.Toast;
 
+import com.google.firebase.database.DataSnapshot;
+import com.google.firebase.database.DatabaseError;
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.FirebaseDatabase;
+import com.google.firebase.database.ValueEventListener;
+import com.satya.uas.Model.Security;
+import com.satya.uas.Model.User;
 import com.satya.uas.R;
+import com.satya.uas.fargment.BiodataFargment;
 
 public class LoginActivity extends AppCompatActivity {
+
+    EditText tUsername, tPassword;
+    Button btnLogin;
+    DatabaseReference databaseReference;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_login);
+
+        tUsername = findViewById(R.id.edit_username);
+        tPassword = findViewById(R.id.edt_password_login);
+        btnLogin = findViewById(R.id.btn_login);
+        databaseReference = FirebaseDatabase.getInstance().getReference("User");
+        btnLogin.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                String pwd = null;
+                try {
+                    pwd = Security.encrypt(tPassword.getText().toString());
+                } catch (Exception e) {
+                    e.printStackTrace();
+                }
+                logIn(tUsername.getText().toString(), pwd);
+            }
+        });
+    }
+
+    private void logIn(final String username, final String password) {
+        databaseReference.addListenerForSingleValueEvent(new ValueEventListener() {
+            @Override
+            public void onDataChange(@NonNull DataSnapshot dataSnapshot) {
+                if (dataSnapshot.child(username).exists()) {
+                    if (!username.isEmpty()) {
+                        User login = dataSnapshot.child(username).getValue(User.class);
+                        if (login.getPassword().equals(password)) {
+                            Toast.makeText(LoginActivity.this, "Login Success", Toast.LENGTH_LONG).show();
+                            Intent intphto = new Intent(getApplicationContext(), SplashActivity.class);
+                            startActivity(intphto);
+                        } else {
+                            Toast.makeText(LoginActivity.this, "Password Incorrect", Toast.LENGTH_LONG).show();
+                        }
+                    } else {
+                        Toast.makeText(LoginActivity.this, "User is not register", Toast.LENGTH_LONG).show();
+                    }
+
+                } else {
+                    Toast.makeText(LoginActivity.this, "User is not register", Toast.LENGTH_LONG).show();
+                }
+            }
+
+            @Override
+            public void onCancelled(@NonNull DatabaseError databaseError) {
+
+            }
+        });
     }
 
     public void clickSignUp(View view) {
